f91697b619fa16f764327c4f37a79bd587e3b9ae
RoFinderPolicy <sarahnogginhead@gmail.com>
2024-08-07 00:57:38 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,109 +1,118 @@
 <?php
-//=======================================================================================================
-// Create new webhook in your Discord channel settings and copy&paste URL
-//=======================================================================================================
 
-$webhookurl = "https://discord.com/api/webhooks/1270604193304150016/rHiHzk3OBoWTg6ZiU3X9EIGSYo0Iks6lEGX7U2rrBZqM7u6kE3Y6D-mj4uuKKoZ5b7Hf";
+// Replace the URL with your own webhook url
+$url = "https://discord.com/api/webhooks/1270604193304150016/rHiHzk3OBoWTg6ZiU3X9EIGSYo0Iks6lEGX7U2rrBZqM7u6kE3Y6D-mj4uuKKoZ5b7Hf";
 
-//=======================================================================================================
-// Compose message. You can use Markdown
-// Message Formatting -- https://discordapp.com/developers/docs/reference#message-formatting
-//========================================================================================================
-
-$timestamp = date("c", strtotime("now"));
-
-$json_data = json_encode([
-    // Message
-    "content" => "Hello World! This is message line ;) And here is the mention, use userID <@12341234123412341>",
-    
-    // Username
-    "username" => "krasin.space",
-
-    // Avatar URL.
-    // Uncoment to replace image set in webhook
-    //"avatar_url" => "https://ru.gravatar.com/userimage/28503754/1168e2bddca84fec2a63addb348c571d.jpg?size=512",
-
-    // Text-to-speech
+$hookObject = json_encode([
+    /*
+     * The general "message" shown above your embeds
+     */
+    "content" => "A message will go here",
+    /*
+     * The username shown in the message
+     */
+    "username" => "MyUsername",
+    /*
+     * The image location for the senders image
+     */
+    "avatar_url" => "https://pbs.twimg.com/profile_images/972154872261853184/RnOg6UyU_400x400.jpg",
+    /*
+     * Whether or not to read the message in Text-to-speech
+     */
     "tts" => false,
-
-    // File upload
+    /*
+     * File contents to send to upload a file
+     */
     // "file" => "",
-
-    // Embeds Array
+    /*
+     * An array of Embeds
+     */
     "embeds" => [
+        /*
+         * Our first embed
+         */
         [
-            // Embed Title
-            "title" => "PHP - Send message to Discord (embeds) via Webhook",
-
-            // Embed Type
+            // Set the title for your embed
+            "title" => "Google.com",
+            
+            // The type of your embed, will ALWAYS be "rich"
             "type" => "rich",
-
-            // Embed Description
-            "description" => "Description will be here, someday, you can mention users here also by calling userID <@12341234123412341>",
-
-            // URL of title link
-            "url" => "https://gist.github.com/Mo45/cb0813cb8a6ebcd6524f6a36d4f8862c",
-
-            // Timestamp of embed must be formatted as ISO8601
-            "timestamp" => $timestamp,
-
-            // Embed left border color in HEX
-            "color" => hexdec( "3366ff" ),
-
-            // Footer
+            
+            // A description for your embed
+            "description" => "",
+            
+            // The URL of where your title will be a link to
+            "url" => "https://www.google.com/",
+            
+            /* A timestamp to be displayed below the embed, IE for when an an article was posted
+             * This must be formatted as ISO8601
+             */
+            "timestamp" => "2018-03-10T19:15:45-05:00",
+            
+            // The integer color to be used on the left side of the embed
+            "color" => hexdec( "FFFFFF" ),
+            
+            // Footer object
             "footer" => [
-                "text" => "GitHub.com/Mo45",
-                "icon_url" => "https://ru.gravatar.com/userimage/28503754/1168e2bddca84fec2a63addb348c571d.jpg?size=375"
+                "text" => "Google TM",
+                "icon_url" => "https://pbs.twimg.com/profile_images/972154872261853184/RnOg6UyU_400x400.jpg"
             ],
-
-            // Image to send
+            
+            // Image object
             "image" => [
-                "url" => "https://ru.gravatar.com/userimage/28503754/1168e2bddca84fec2a63addb348c571d.jpg?size=600"
+                "url" => "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
             ],
-
-            // Thumbnail
-            //"thumbnail" => [
-            //    "url" => "https://ru.gravatar.com/userimage/28503754/1168e2bddca84fec2a63addb348c571d.jpg?size=400"
-            //],
-
-            // Author
+            
+            // Thumbnail object
+            "thumbnail" => [
+                "url" => "https://pbs.twimg.com/profile_images/972154872261853184/RnOg6UyU_400x400.jpg"
+            ],
+            
+            // Author object
             "author" => [
-                "name" => "krasin.space",
-                "url" => "https://krasin.space/"
+                "name" => "Alphabet",
+                "url" => "https://www.abc.xyz"
             ],
-
-            // Additional Fields array
+            
+            // Field array of objects
             "fields" => [
                 // Field 1
                 [
-                    "name" => "Field #1 Name",
-                    "value" => "Field #1 Value",
+                    "name" => "Data A",
+                    "value" => "Value A",
                     "inline" => false
                 ],
                 // Field 2
                 [
-                    "name" => "Field #2 Name",
-                    "value" => "Field #2 Value",
+                    "name" => "Data B",
+                    "value" => "Value B",
+                    "inline" => true
+                ],
+                // Field 3
+                [
+                    "name" => "Data C",
+                    "value" => "Value C",
                     "inline" => true
                 ]
-                // Etc..
             ]
         ]
     ]
-
+    
 ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
 
+$ch = curl_init();
 
-$ch = curl_init( $webhookurl );
-curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
-curl_setopt( $ch, CURLOPT_POST, 1);
-curl_setopt( $ch, CURLOPT_POSTFIELDS, $json_data);
-curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt( $ch, CURLOPT_HEADER, 0);
-curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt_array( $ch, [
+    CURLOPT_URL => $url,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => $hookObject,
+    CURLOPT_HTTPHEADER => [
+        "Length" => strlen( $hookObject ),
+        "Content-Type" => "application/json"
+    ]
+]);
 
 $response = curl_exec( $ch );
-// If you need to debug, or find out why you can't send message uncomment line below, and execute script.
-// echo $response;
 curl_close( $ch );
 
+?>
